0f8e2c393714bbf827f831b3ad383c976af0ab4b
DENIS BLINOV <blinovda1@mail.ru>
2022-05-26 06:36:12 +0300

    add task4

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,16 @@
-﻿// 43
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4->24
+// 5->120
+
+int SumNum(int num)
+{
+    int all_sum = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        all_sum *= i;
+    }
+    return all_sum;
+}
+
+Console.WriteLine(SumNum(4));
+Console.WriteLine(SumNum(5));
