0ecb8734abbcd72988e3793f7c8650e8b30d47ed
DENIS BLINOV <blinovda1@mail.ru>
2022-05-26 07:08:37 +0300

    add task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Выводит массив из 8 элементов, заполненый нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int [] array = new int[8];
+for (int i=0; i<8; i++)
+{
+    array[i]=new Random().Next(2);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
